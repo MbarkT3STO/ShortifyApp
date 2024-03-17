1291a1a4b33ec217a009328bf89fd6f9ef3998be
mbarkt3sto <mbark1236@gmail.com>
2024-03-17 18:02:39 +0000

    Chore: Chore Commit

--- source/Shared/Shared.Database.RPC/obj/Debug/net8.0/Shared.Database.RPC.AssemblyInfo.cs
+++ source/Shared/Shared.Database.RPC/obj/Debug/net8.0/Shared.Database.RPC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shared.Database.RPC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf648444de984e160f7d24d67d187894ce511aef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4ff0c60d8338917ea71e6976d9503c11c0edace")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shared.Database.RPC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shared.Database.RPC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
